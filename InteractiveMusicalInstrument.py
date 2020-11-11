068a5fe465c505e703567129f701b638e7c76af0
Kongpob <kongpop.rak@hotmail.co.th>
2020-11-11 16:32:03 +0700

    clean up unuse code

--- InteractiveMusicalInstrument.py
+++ InteractiveMusicalInstrument.py
@@ -71,7 +71,6 @@ clock = pygame.time.Clock()
 running = True
 configMode = False
 lastGameTicks = 0
-pianoKeys = pygame.image.load('./src/piano/keys.png')
 keyEvents = [pygame.K_q,pygame.K_w,pygame.K_e,pygame.K_r,pygame.K_t,pygame.K_a,pygame.K_s,pygame.K_d,pygame.K_f,pygame.K_g,pygame.K_z,pygame.K_x,pygame.K_c,pygame.K_v,pygame.K_b]
 keyPosXs = [30,85,140,195,250,30,85,140,195,250,30,85,140,195,250]
 keyPosYs = [400,400,400,400,400,455,455,455,455,455,510,510,510,510,510]
